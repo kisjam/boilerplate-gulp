c66a6c0a7b5d0b16e7a3c89d803d428b6d3007de
kisjam <kisjam@gmail.com>
2013-09-03 16:21:19 +0900

    ok

--- pc/include/common/Head.php
+++
@@ -1,5 +0,0 @@
-	<meta name="description" content="サイトの説明文">
-	<meta name="keywords" content="サイトのキーワード">
-	<meta name="viewport" content="width=1024">
-	<link rel="index" href="/" title="Home">
-	<link rel="shortcut icon" type="image/x-icon" href="/common/images/favicon.ico">
--- pc/include/common/Js.php
+++
@@ -1,3 +0,0 @@
-	<!--[if lt IE 9]><script src="/common/js/html5shiv-printshiv.js"></script><![endif]-->
-	<script src="<?php get_common_dir('js'); ?>require.js"></script>
-	<script src="<?php get_common_dir('js'); ?>main.js"></script>
