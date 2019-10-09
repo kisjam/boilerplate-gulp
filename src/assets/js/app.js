ccff15fe732a54c84bc8b449b618e6f46125c0de
kisjam <kisjam@gmail.com>
2019-10-09 16:56:08 +0900

    bug fix

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,12 +4,8 @@ import ScrollAnimation from './modules/scroll-animation';
 import mobileNavigation from './modules/mobile-menu';
 import './modules/smooth-scroll';
 import throttle from 'lodash.throttle';
-const navStatus = require('./modules/nav-status');
-const mobileNavigation = require('./modules/mobile-menu');
-require('./modules/utility');
-require('./modules/scroll-animation');
-require('./modules/response-image');
-require('./modules/smooth-scroll');
+
+
 
 window.addEventListener('DOMContentLoaded', () => {
 
