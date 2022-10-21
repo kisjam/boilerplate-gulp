3115897b1f61a5159df61c7106064e0c82ab10f0
kisjam <kisjam@gmail.com>
2022-10-21 17:47:40 +0900

    微調整

--- src/assets/js/modules/accordion.ts
+++ src/assets/js/modules/accordion.ts
@@ -33,9 +33,7 @@ export default class Accordion {
 		document.addEventListener('DOMContentLoaded', () => {
 			const elems = document.querySelectorAll<HTMLDetailsElement>(this.option.selector);
 
-			if (elems === null) return;
-
-			elems.forEach((elem) => {
+			elems?.forEach((elem) => {
 				this.registEventHandler(elem);
 			})
 		})
